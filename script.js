7c4e8745fd965ffef320e2b778d2d9461bfe703a
Sergej Furdulovic <sergejfurdulovic@gmail.com>
2025-04-22 16:41:56 +0200

    slider

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-alert("Hello World");
-console.log("bilo sta");
+const contactTrack = document.querySelector(".marquee-slider");
+const contactItems = Array.from(contactTrack.children);
+contactItems.forEach((contactItem) => {
+  const contactClone = contactItem.cloneNode(true);
+  contactTrack.appendChild(contactClone);
+});
